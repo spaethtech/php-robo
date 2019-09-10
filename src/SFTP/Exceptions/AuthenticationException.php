da9a6459dfad3c861300aa734ec6e8cef69d021e
rspaeth79 <rspaeth@mvqn.net>
2019-09-10 12:35:28 -0700

    - Moved SFTP code to mvqn/sftp project.

--- src/SFTP/Exceptions/AuthenticationException.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace MVQN\SFTP\Exceptions;
-
-final class AuthenticationException extends \Exception
-{
-}
--- src/SFTP/Exceptions/RemoteConnectionException.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace MVQN\SFTP\Exceptions;
-
-final class RemoteConnectionException extends \Exception
-{
-}
